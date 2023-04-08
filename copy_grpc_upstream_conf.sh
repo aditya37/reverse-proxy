ba7163a427c4fb8a5f48a17088645fdefb0f11b5
Aditya Kurnia Rahman <aditya.krohman@gmail.com>
2023-04-08 17:17:42 +0700

    fixing

--- copy_grpc_upstream_conf.sh
+++ copy_grpc_upstream_conf.sh
@@ -6,7 +6,7 @@ set -o pipefail
 
 if [[ $GRPC_CONF_UPSTREAM_PATH != "" ]] 
 then
-    for f in ./api_upstream.d/*.conf; do 
+    for f in ./grpc_upstream.d/*.conf; do 
         echo "copying file to"
         echo $GRPC_CONF_UPSTREAM_PATH${f##*/}
         cp $f $GRPC_CONF_UPSTREAM_PATH${f##*/}
